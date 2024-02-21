b77716d91b296768374f77037a33650e566273f8
John Shields <shields101@hotmail.com>
2024-02-21 21:16:43 +0000

    Create base file for templates

--- run.py
+++ run.py
@@ -12,9 +12,15 @@ def index():
 def about():
     return render_template("about.html")
 
+
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
+
+
+@app.route("/careers")
+def careers():
+    return render_template("careers.html")
 
 
 if __name__ == "__main__":
